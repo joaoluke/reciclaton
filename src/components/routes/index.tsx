c8575c58267584de5b6bf0725504c64e0824a486
Cauan França <cauan.f.ramos@gmail.com>
2020-09-29 10:00:30 -0300

    docs: codereview done

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -2,22 +2,24 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 const Routes = () => {
-  return <>
-    <Switch>
-      <Route exact path="/">
-        *PAGINA INICIAL*
-    </Route>
-      <Route exact path="/login">
-        *PAGINA DE LOGIN*
-    </Route>
-      <Route exact path="/ranking">
-        *PAGINA DE RANKING*
-    </Route>
-      <Route exact path="/profile">
-        *PAGINA DE PROFILE*
-    </Route>
-    </Switch>
-  </>
-}
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          *PAGINA INICIAL*
+        </Route>
+        <Route exact path="/login">
+          *PAGINA DE LOGIN*
+        </Route>
+        <Route exact path="/ranking">
+          *PAGINA DE RANKING*
+        </Route>
+        <Route exact path="/profile">
+          *PAGINA DE PROFILE*
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default Routes;
