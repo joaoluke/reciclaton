ea70377a14bf61ca345141f42a65b97c68c22213
Felipe <felipematheusmellodemorais@gmail.com  name = Felipe Matheus Mello de Morais>
2020-09-30 11:01:11 -0300

    docs: mounting header

--- src/components/layout/styled-layout.tsx
+++ src/components/layout/styled-layout.tsx
@@ -1,1 +1,19 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+
+export const Logo = styled.div`
+  margin: 0 10px;
+  width: 35px;
+  height: 40px;
+  background-position: -1px;
+  background-image: url(${LogoResponsivo});
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media screen and (max-width: 540px) {
+    width: 30px;
+    height: 35px;
+    background-position: -1px;
+    background-image: url(${LogoResponsivo});
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+`;
