3628e267385d492b6920f36fc67f36eebb59bb74
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2020-10-05 16:53:53 -0300

    feat: JWT decode implemented

--- src/pages/perfil-user/index.jsx
+++ src/pages/perfil-user/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import decode from 'jwt-decode';
 import { getPerfil } from '../../redux/action/user';
+
 
 
 const Perfil = () => {
@@ -10,15 +12,16 @@ const Perfil = () => {
   const { userId } = useParams();
   let { user } = useSelector((state) => state.user);
   let logged = useSelector((state) => state.login);
+  const decoded = decode(logged.authen)
   useEffect(() => dispatch(getPerfil(userId)),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [userId])
 
-  console.log(logged)
-  console.log(user)
+  console.log(decoded.sub === userId)
   return (<>
 
-    <h1>{JSON.stringify(user)}</h1>
+    {decoded.sub === userId && <h1>usuario no path logado aqui</h1>}
+    {decoded.sub !== userId && <h1>usuario no path não está logado aqui</h1>}
 
   </>
   )
