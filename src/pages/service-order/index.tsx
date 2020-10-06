04ef96bfa8e0ac1f154308d13759c445ed6a1e18
rafael-feliciano <rafael.cefetpg@gmail.com>
2020-10-06 09:16:11 -0300

    feat: axios

--- src/pages/service-order/index.tsx
+++ src/pages/service-order/index.tsx
@@ -1,8 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { StyledButton, ContainerButton } from "./styled";
 
 const ServiceOrder = () => {
   const [status, setStatus] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://reciclatonapi.herokuapp.com/664/users/1`, {
+        headers: {
+          Authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhZmFlbEByYWZhZWwuY29tLmJyIiwiaWF0IjoxNjAxOTI5NTY5LCJleHAiOjE2MDE5MzMxNjksInN1YiI6Ijg3MiJ9.NXsfWJRnccxep1dHzq7vyIjit6J7zsSi2AxYqXbhJYI",
+        },
+      })
+
+      .then((resp) => {
+        setData(resp.data.business);
+        console.log("data: " + data);
+      })
+      .catch((error) => {
+        // erro no request
+        console.log(error);
+      });
+  }, [status]);
+
+  console.log("data: " + data);
+
   return (
     <>
       <ContainerButton>
