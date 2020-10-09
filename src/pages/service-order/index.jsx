fc3457b783e3a4cbfc8ad850673284760d8ecf5c
rafael-feliciano <rafael.cefetpg@gmail.com>
2020-10-09 16:38:44 -0300

    refactor: pull conflict

--- src/pages/service-order/index.jsx
+++ src/pages/service-order/index.jsx
@@ -10,7 +10,6 @@ const ServiceOrder = () => {
   const [userId, setUserId] = useState("");
   const dispatch = useDispatch();
   const [status, setStatus] = useState("");
-<<<<<<< HEAD
   const business = useSelector((state) => state.user.business);
   const os = useSelector((state) => state.user.os);
   const token = useSelector((state) => state.authentication);
@@ -20,19 +19,6 @@ const ServiceOrder = () => {
     token == !undefined && dispatch(requestBusiness(userId.sub, token));
   }, []);
 
-=======
-  const business = useSelector((state) => state.user);
-  const token = useSelector((state) => state.login.authen);
-
-  const token2 =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJhdGF0aW5oYUBnbWFpbC5jb20iLCJpYXQiOjE2MDIwODE4NTYsImV4cCI6MTYwMjA4NTQ1Niwic3ViIjoiODczIn0.fZctgbVbNT9zSkNUGvArCm8J4YC5PeutwEqyTZBMkEM";
-  const userId = parseInt(jwt_decode(token2).sub);
-
-  useEffect(() => {}, [status]);
-  dispatch(requestBusiness(userId, token2));
-  console.log(userId);
-  console.log(business);
->>>>>>> ac8c6c84a7d2bc23125b2fa21598b7970db6e260
   return (
     <>
       {console.log(token)}
