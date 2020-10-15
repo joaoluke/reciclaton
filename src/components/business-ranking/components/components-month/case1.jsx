d6e92627bd055e71f47ba0d34acb53037ae65c05
joaoluke <joaolucas.deoliveira56@gmail.com>
2020-10-15 01:16:49 +0100

    feat: test

--- src/components/business-ranking/components/components-month/case1.jsx
+++ src/components/business-ranking/components/components-month/case1.jsx
@@ -14,7 +14,7 @@ const Case1 = ({
   bronzeHonor,
   orderByScoreMonth,
   currentBusiness,
-  lastBusinessRefElement
+  lastBusinessRefElement,
 }) => {
   const history = useHistory();
   return (
@@ -25,121 +25,128 @@ const Case1 = ({
             if (currentBusiness.length === index + 1) {
               return (
                 <div ref={lastBusinessRefElement}>
-            <div key={index} icon={index + 1 + "ª"}>
-              <div>
-                {index === 0 && (
-                  <img
-                    src={goldTrophy}
-                    style={{ width: "50px", height: "50px" }}
-                  />
-                )}
-                {index === 1 && (
-                  <img
-                    src={silverTrophy}
-                    style={{ width: "45px", height: "45px" }}
-                  />
-                )}
-                {index === 2 && (
-                  <img
-                    src={bronzeTrophy}
-                    style={{ width: "40px", height: "40px" }}
-                  />
-                )}
-                {index <= business.sort(orderByScoreMonth).length / 3 &&
-                  index > 2 && (
-                    <img src={goldHonor} style={{ width: "30px" }} />
-                  )}
-                {index > business.sort(orderByScoreMonth).length / 3 &&
-                  index < (2 * business.sort(orderByScoreMonth).length) / 3 && (
-                    <img src={silverHonor} style={{ width: "30px" }} />
-                  )}
-                {index >= (2 * business.sort(orderByScoreMonth).length) / 3 &&
-                  index <=
-                    (3 * business.sort(orderByScoreMonth).length) / 3 && (
-                    <img src={bronzeHonor} style={{ width: "30px" }} />
-                  )}
-              </div>
-              <div
-                onClick={() => history.push(`users/${item.id}`)}
-                style={{ color: "green", fontWeight: "bolder" }}
-              >
-                {item.brand}
-              </div>
-              <div>
-                <a href={item.website}>{item.website}</a>
-              </div>
-              <div>Categoria: {item.business}</div>
-              <div>
-                <span>Porte: {item.businessSize}</span>
-              </div>
-              <div>
-                <img
-                  src={item.imageUrl}
-                  style={{ width: "40px", borderRadius: "10px" }}
-                />
-              </div>
-              <div style={{ color: "#000" }}>{item.score.mensal} Score</div>
-            </div>
-            </div>
-              )} else {
-                return (
                   <div key={index} icon={index + 1 + "ª"}>
-              <div>
-                {index === 0 && (
-                  <img
-                    src={goldTrophy}
-                    style={{ width: "50px", height: "50px" }}
-                  />
-                )}
-                {index === 1 && (
-                  <img
-                    src={silverTrophy}
-                    style={{ width: "45px", height: "45px" }}
-                  />
-                )}
-                {index === 2 && (
-                  <img
-                    src={bronzeTrophy}
-                    style={{ width: "40px", height: "40px" }}
-                  />
-                )}
-                {index <= business.sort(orderByScoreMonth).length / 3 &&
-                  index > 2 && (
-                    <img src={goldHonor} style={{ width: "30px" }} />
-                  )}
-                {index > business.sort(orderByScoreMonth).length / 3 &&
-                  index < (2 * business.sort(orderByScoreMonth).length) / 3 && (
-                    <img src={silverHonor} style={{ width: "30px" }} />
-                  )}
-                {index >= (2 * business.sort(orderByScoreMonth).length) / 3 &&
-                  index <=
-                    (3 * business.sort(orderByScoreMonth).length) / 3 && (
-                    <img src={bronzeHonor} style={{ width: "30px" }} />
-                  )}
-              </div>
-              <div
-                onClick={() => history.push(`users/${item.id}`)}
-                style={{ color: "green", fontWeight: "bolder" }}
-              >
-                {item.brand}
-              </div>
-              <div>
-                <a href={item.website}>{item.website}</a>
-              </div>
-              <div>Categoria: {item.business}</div>
-              <div>
-                <span>Porte: {item.businessSize}</span>
-              </div>
-              <div>
-                <img
-                  src={item.imageUrl}
-                  style={{ width: "40px", borderRadius: "10px" }}
-                />
-              </div>
-              <div style={{ color: "#000" }}>{item.score.mensal} Score</div>
-            </div>
-                )
-              }
+                    <div>
+                      {index === 0 && (
+                        <img
+                          src={goldTrophy}
+                          style={{ width: "50px", height: "50px" }}
+                        />
+                      )}
+                      {index === 1 && (
+                        <img
+                          src={silverTrophy}
+                          style={{ width: "45px", height: "45px" }}
+                        />
+                      )}
+                      {index === 2 && (
+                        <img
+                          src={bronzeTrophy}
+                          style={{ width: "40px", height: "40px" }}
+                        />
+                      )}
+                      {index <= business.sort(orderByScoreMonth).length / 3 &&
+                        index > 2 && (
+                          <img src={goldHonor} style={{ width: "30px" }} />
+                        )}
+                      {index > business.sort(orderByScoreMonth).length / 3 &&
+                        index <
+                          (2 * business.sort(orderByScoreMonth).length) / 3 && (
+                          <img src={silverHonor} style={{ width: "30px" }} />
+                        )}
+                      {index >=
+                        (2 * business.sort(orderByScoreMonth).length) / 3 &&
+                        index <=
+                          (3 * business.sort(orderByScoreMonth).length) / 3 && (
+                          <img src={bronzeHonor} style={{ width: "30px" }} />
+                        )}
+                    </div>
+                    <div
+                      onClick={() => history.push(`users/${item.id}`)}
+                      style={{ color: "green", fontWeight: "bolder" }}
+                    >
+                      {item.brand}
+                    </div>
+                    <div>
+                      <a href={item.website}>{item.website}</a>
+                    </div>
+                    <div>Categoria: {item.business}</div>
+                    <div>
+                      <span>Porte: {item.businessSize}</span>
+                    </div>
+                    <div>
+                      <img
+                        src={item.imageUrl}
+                        style={{ width: "40px", borderRadius: "10px" }}
+                      />
+                    </div>
+                    <div style={{ color: "#000" }}>
+                      {item.score.mensal} Score
+                    </div>
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <div key={index} icon={index + 1 + "ª"}>
+                  <div>
+                    {index === 0 && (
+                      <img
+                        src={goldTrophy}
+                        style={{ width: "50px", height: "50px" }}
+                      />
+                    )}
+                    {index === 1 && (
+                      <img
+                        src={silverTrophy}
+                        style={{ width: "45px", height: "45px" }}
+                      />
+                    )}
+                    {index === 2 && (
+                      <img
+                        src={bronzeTrophy}
+                        style={{ width: "40px", height: "40px" }}
+                      />
+                    )}
+                    {index <= business.sort(orderByScoreMonth).length / 3 &&
+                      index > 2 && (
+                        <img src={goldHonor} style={{ width: "30px" }} />
+                      )}
+                    {index > business.sort(orderByScoreMonth).length / 3 &&
+                      index <
+                        (2 * business.sort(orderByScoreMonth).length) / 3 && (
+                        <img src={silverHonor} style={{ width: "30px" }} />
+                      )}
+                    {index >=
+                      (2 * business.sort(orderByScoreMonth).length) / 3 &&
+                      index <=
+                        (3 * business.sort(orderByScoreMonth).length) / 3 && (
+                        <img src={bronzeHonor} style={{ width: "30px" }} />
+                      )}
+                  </div>
+                  <div
+                    onClick={() => history.push(`users/${item.id}`)}
+                    style={{ color: "green", fontWeight: "bolder" }}
+                  >
+                    {item.brand}
+                  </div>
+                  <div>
+                    <a href={item.website}>{item.website}</a>
+                  </div>
+                  <div>Categoria: {item.business}</div>
+                  <div>
+                    <span>Porte: {item.businessSize}</span>
+                  </div>
+                  <div>
+                    <img
+                      src={item.imageUrl}
+                      style={{ width: "40px", borderRadius: "10px" }}
+                    />
+                  </div>
+                  <div style={{ color: "#000" }}>{item.score.mensal} Score</div>
+                </div>
+              );
+            }
           })}
         </Timeline>
       </StyleRoot>
